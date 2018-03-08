23e5416309791bed812f9568494ff836ccfb448b
ajreno95 <ajreno952@gmail.com>
2018-03-08 00:42:14 -0500

    Added convertImageToBitmap and displayImage methods

--- app/src/main/java/com/example/alexander/image/MainActivity.java
+++ app/src/main/java/com/example/alexander/image/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.alexander.image;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Resources res = getResources();
-        Drawable drawable = res.getDrawable(R.drawable.ximage);
 
+
+    }
+
+    private Bitmap convertImageToBitmap(Drawable image, int x, int y){
+        //convert image to bitmap then resize
+        Bitmap bitmapImage = ((BitmapDrawable)image).getBitmap();
+        Bitmap resizedImage = Bitmap.createScaledBitmap(bitmapImage, x, y, false);
+
+        return resizedImage;
+    }
+
+    private void displayImage(Drawable image){
+        ImageView imageView = (ImageView) findViewById(R.id.imageDisplay);
+        imageView.setImageDrawable(image);
     }
 
 }
